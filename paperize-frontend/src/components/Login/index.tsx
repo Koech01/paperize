08691faf6173b2c9f83b1d9e6617e76ef4843278
Koech01 <01koech@gmail.com>
2025-02-22 19:08:58 +0300

    Styling fonts and form link

--- paperize-frontend/src/components/Login/index.tsx
+++ paperize-frontend/src/components/Login/index.tsx
@@ -4,10 +4,11 @@ const Login = () => {
     return(
         <div className={css.authParentDiv}> 
             <form className={css.authForm}>
-                <h2>Log in</h2>
+                <h2 className={css.authFormHeader}>Log in</h2>
                 <input className={css.authFormInput} required autoComplete="off" placeholder="Email" type="email"/>
                 <input className={css.authFormInput} required autoComplete="off" placeholder="Password" type="password"/>
                 <button className={css.authFormBtn} type="submit">Login</button>
+                <div className={css.authFormLinkDiv}><a className={css.authFormLink}>Forgot Password ?</a></div>
             </form> 
         </div>
     );
