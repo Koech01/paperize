dab5b01e0b0035b6a33e470de7eebe412fa6ac65
Koech01 <01koech@gmail.com>
2025-02-26 16:18:09 +0300

    Styling home component overlay

--- paperize-frontend/src/components/Home/index.tsx
+++ paperize-frontend/src/components/Home/index.tsx
@@ -1,10 +1,27 @@
-import css from './index.module.css';
+import css from './index.module.css'; 
 
 
 const Home = () => {
     
     return(
-        <div className={css.homaParentDiv}></div>
+        <div className={css.homeParentDiv}> 
+            <div className={css.homeChildDiv}>
+                <div className={css.homeNavbarDiv}> 
+                    <nav className={css.homeNavbar}>
+                        <ul className={css.homeUnorderList}>
+                            <li className={`${css.homeNavbarItem} ${css.active}`}>Home</li>
+                            <li className={css.homeNavbarItem}>Create</li>
+                            <li className={css.homeNavbarItem}>Report</li>
+                            <li className={css.homeNavbarItem}>Settings</li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div className={css.homeComponentDiv}>
+
+                </div>
+            </div> 
+        </div>
     );
 }
 
