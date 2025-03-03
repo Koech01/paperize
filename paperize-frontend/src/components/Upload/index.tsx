bc3d9d3ce1edacd735d5d228f57f71f873eebc4e
Koech01 <01koech@gmail.com>
2025-03-03 16:07:14 +0300

    Styling upload document component

--- paperize-frontend/src/components/Upload/index.tsx
+++ paperize-frontend/src/components/Upload/index.tsx
@@ -1,33 +1,43 @@
 import css from './index.module.css';
 import docIcon from '../assets/docIcon.svg';
+import closeLightIcon from '../assets/closeLight.svg'; 
+import folderLightIcon from '../assets/folderLightIcon.png';
+
 
 const Upload = () => {
 
     return(
         <div className={css.uploadParentDiv}> 
 
-            <div>
-                <input type="file"/>
+            <div className={css.uploadChildDiv}>
+                <div className={css.uploadInputDiv}>
+                    <img className={css.uploaFolderIcon} src={folderLightIcon} alt='create-doc-icon'/>
+                    <p className={css.uploadFileName}>Choose a file to upload</p>
+                    <input className={css.uploadInput} type="file"/>
+                </div>
 
                 <div className={css.uploadFileItemListDiv}>
 
                     <div className={css.uploadFileItemDiv}>  
                         <div className={css.uploadFileItemChildDiv}>
-                            <img className={css.uploadFileIcon} src={docIcon} alt='create-doc-icon' />
+                            <img className={css.uploadFileIcon} src={docIcon} alt='create-doc-icon'/>
 
                             <div className={css.uploadFileItemTextDiv}> 
-                                <p>my-cv.docx</p>
+                                <p className={css.uploadFileName}>Using Relative Positioning</p>
 
                                 <div className={css.uploadFileItemStatusDiv}>
-                                    <p>100 MB &nbsp;.</p>
-                                    <p>&nbsp;Uploading...</p>
+                                    <p className={css.uploadFileText}>XLSX&nbsp;&nbsp;</p>
+                                    <p className={css.uploadFileText}>100 MB&nbsp;&nbsp;<span className={css.uploadFileDot}>.</span></p>
+                                    <p className={css.uploadFileText}>&nbsp;&nbsp;Uploading...</p>
                                 </div>
                             </div>
     
-                            <button>cancel</button>
+                            <button className={css.uploadFileCancelBtn}>
+                                <img className={css.uploadFileCancelIcon} src={closeLightIcon} alt='upload-cancel-btn-icon'/>
+                            </button>
                         </div>
 
-                        <div className={css.uploadFileItemChildDiv}>
+                        <div className={css.uploadFileLoadingDiv}>
                             {/* Loading  */}
                         </div>
                     </div>
@@ -37,18 +47,21 @@ const Upload = () => {
                             <img className={css.uploadFileIcon} src={docIcon} alt='create-doc-icon' />
 
                             <div className={css.uploadFileItemTextDiv}> 
-                                <p>my-cv.docx</p>
+                                <p className={css.uploadFileName}>Using Relative Positioning</p>
 
                                 <div className={css.uploadFileItemStatusDiv}>
-                                    <p>100 MB &nbsp;.</p>
-                                    <p>&nbsp;Completed</p>
+                                    <p className={css.uploadFileText}>XLSX&nbsp;&nbsp;</p>
+                                    <p className={css.uploadFileText}>100 MB&nbsp;&nbsp;<span className={css.uploadFileDot}>.</span></p>
+                                    <p className={css.uploadFileText}>&nbsp;&nbsp;Completed</p>
                                 </div>
                             </div>
     
-                            <button>cancel</button>
+                            <button className={css.uploadFileCancelBtn}>
+                                <img className={css.uploadFileCancelIcon} src={closeLightIcon} alt='upload-cancel-btn-icon'/>
+                            </button>
                         </div>
 
-                        <div className={css.uploadFileItemChildDiv}>
+                        <div className={css.uploadFileLoadingDiv}>
                             {/* Loading  */}
                         </div>
                     </div>
