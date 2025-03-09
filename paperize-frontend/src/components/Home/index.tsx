4014441a6666b81256ae5744e56f7079620ea037
Koech01 <01koech@gmail.com>
2025-03-09 15:33:10 +0300

    Styling home document search input and filter btn

--- paperize-frontend/src/components/Home/index.tsx
+++ paperize-frontend/src/components/Home/index.tsx
@@ -7,6 +7,7 @@ import cssIcon from '../assets/css.png';
 import datIcon from '../assets/dat.png';
 import docxIcon from '../assets/docx.png';
 import { useEffect, useState } from 'react';
+import homeFilterLightIcon from '../assets/homeFilterLightIcon.svg';
 
 
 const Home = () => {
@@ -24,84 +25,97 @@ const Home = () => {
         <div className={`${css.homeParentDiv} ${theme === 'light' ? css.lightTheme : css.darkTheme}`}> 
             <div className={css.homeChildDiv}>
  
-                <div className={css.homeDocumentsUtilityBar}>  
-                    <ul className={css.homeDocumentBreadcrumbs}>
-                        <li><a href="#">Modular Cloud</a></li>
-                        <li><a href="#">Celestia Mainnet</a></li>
-                        <li><a href="#">Transactions</a></li> 
-                    </ul>
+                <div className={css.homeDocumentsComponentDiv}>  
+                    <div className={css.homeDocumentsUtilityBar}>  
+                        <ul className={css.homeDocumentBreadcrumbs}>
+                            <li><a href="#">Modular Cloud</a></li>
+                            <li><a href="#">Celestia Mainnet</a></li>
+                            <li><a href="#">Transactions</a></li> 
+                        </ul>
 
-                    <div className={css.homeDocumentsUtilityFlexDiv}>
-                        <input className={css.homeDocumentsUtilitySearchBar} type="text"/>
-                        <button className={css.homeDocumentsUtilityFilterBtn}>Filter</button>
-                    </div>
-                </div> 
-
-                <div className={css.homeDocumentsItemLabel}>  
-                    <p className={css.homeDocumentLabelName}>File Name</p> 
-                    <p className={css.homeDocumentLabelSize}>Size</p>
-                    <p className={css.homeDocumentLabelDate}>Date</p>
-                </div> 
-
-                <div className={css.homeDocumentsList}>
-                     
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={sevenzIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
+                        <div className={css.homeDocumentsUtilityFlexDiv}>
+                            <input  
+                                type        = 'text' 
+                                placeholder = 'Search...'
+                                className   = {css.homeDocumentSearchInput} 
+                            />
+                            <button className={css.homeDocumentFilterBtn}>
+                                <img  
+                                    src       = {homeFilterLightIcon} 
+                                    alt       = 'home-filter-btn-icon'
+                                    className = {css.homeDocumentFilterBtnIcon} 
+                                />
+                                Filter
+                            </button>
+                        </div>
                     </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={aacIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
+                    <div className={css.homeDocumentsItemLabel}>  
+                        <p className={css.homeDocumentLabelName}>File Name</p> 
+                        <p className={css.homeDocumentLabelSize}>Size</p>
+                        <p className={css.homeDocumentLabelDate}>Date</p>
                     </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={aviIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                    <div className={css.homeDocumentsList}> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={sevenzIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={bmpIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={aacIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={sevenzIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={aviIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={cssIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={bmpIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={datIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={sevenzIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                    <div className={css.homeDocumentsItem}> 
-                        <img className={css.homeDocumentTypeIcon} src={docxIcon} alt='document-icon'/>
-                        <p className={css.homeDocumentName}>Sales Report</p> 
-                        <p className={css.homeDocumentSize}>350 KB</p>
-                        <p className={css.homeDocumentCreated}>Thursday, 6 March 2025, 13:28 PM</p>
-                    </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={cssIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
 
-                </div> 
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={datIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
+
+                        <div className={css.homeDocumentsItem}> 
+                            <img className={css.homeDocumentTypeIcon} src={docxIcon} alt='document-icon'/>
+                            <p className={css.homeDocumentName}>Sales Report</p> 
+                            <p className={css.homeDocumentSize}>350 KB</p>
+                            <p className={css.homeDocumentCreated}>30 Min Ago</p>
+                        </div> 
+
+                    </div> 
+                </div>
+
            </div>
         </div>
     );
