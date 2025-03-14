f0fc02037ee391c437abb7bd73a64d1dceafc708
Koech01 <01koech@gmail.com>
2025-03-14 20:32:49 +0300

    Creating auth components message templates

--- paperize-frontend/src/components/ResetPassword/index.tsx
+++ paperize-frontend/src/components/ResetPassword/index.tsx
@@ -1,5 +1,7 @@
 import css from '../Login/index.module.css';
 import { useEffect, useState } from 'react'; 
+import authErrorLightIcon from '../assets/authErrorLightIcon.svg';
+import authSuccessLightIcon from '../assets/authSuccessLightIcon.svg';
 
 
 const ResetPassword = () => {
@@ -19,8 +21,17 @@ const ResetPassword = () => {
                 <input className={css.authFormInput} required autoComplete="off" placeholder="Enter Password" type="password"/>
                 <input className={css.authFormInput} required autoComplete="off" placeholder="Confirm Password" type="password"/>
                 <button className={css.authFormBtn} type="submit">Reset</button>
-                <div className={css.authFormErrorDiv}>
-                    <p className={css.authFormErrorText}>Error</p>
+
+                <div className={css.authFormMessageParentDiv}> 
+                    <div className={`${css.authFormMessageDiv} ${css.authSuccess}`}>
+                        <img className={css.authFormSuccessIcon} src={authSuccessLightIcon} alt='auth-message-icon'/> 
+                        <p className={css.authFormMessageText}>Successfully created an account.</p>
+                    </div> 
+
+                    <div className={`${css.authFormMessageDiv} ${css.authError}`}>
+                        <img className={css.authFormSuccessIcon} src={authErrorLightIcon} alt='auth-message-icon'/> 
+                        <p className={css.authFormMessageText}>Successfully created an account.</p>
+                    </div>
                 </div>
             </form> 
         </div>
--- paperize-frontend/src/components/SignUp/index.tsx
+++ paperize-frontend/src/components/SignUp/index.tsx
@@ -1,6 +1,8 @@
 import css from '../Login/index.module.css';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import authErrorLightIcon from '../assets/authErrorLightIcon.svg';
+import authSuccessLightIcon from '../assets/authSuccessLightIcon.svg';
 
 
 const SignUp = () => {
@@ -27,8 +29,16 @@ const SignUp = () => {
                 <button className={css.authFormBtn} type="submit">Sign up</button>
                 <div className={css.authFormLinkDiv}><a className={css.authFormLink} onClick={redirectToLogin}>Log In</a></div>
 
-                <div className={css.authFormErrorDiv}>
-                    <p className={css.authFormErrorText}>Error</p>
+                <div className={css.authFormMessageParentDiv}> 
+                    <div className={`${css.authFormMessageDiv} ${css.authSuccess}`}>
+                        <img className={css.authFormSuccessIcon} src={authSuccessLightIcon} alt='auth-message-icon'/> 
+                        <p className={css.authFormMessageText}>Successfully created an account.</p>
+                    </div> 
+
+                    <div className={`${css.authFormMessageDiv} ${css.authError}`}>
+                        <img className={css.authFormSuccessIcon} src={authErrorLightIcon} alt='auth-message-icon'/> 
+                        <p className={css.authFormMessageText}>Successfully created an account.</p>
+                    </div>
                 </div>
             </form> 
         </div>
