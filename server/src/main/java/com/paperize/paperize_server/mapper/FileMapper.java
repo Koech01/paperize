2b6d08566b7b60e3aa408ed6f54032ff5219ce7e
Muchiri <developedbyrobbie@gmail.com>
2025-03-17 20:09:42 +0300

    Add FolderDto and implement toFolderDto method in FileMapper for folder data handling

--- server/src/main/java/com/paperize/paperize_server/mapper/FileMapper.java
+++ server/src/main/java/com/paperize/paperize_server/mapper/FileMapper.java
@@ -2,8 +2,12 @@ package com.paperize.paperize_server.mapper;
 
 import com.paperize.paperize_server.file.FileEntity;
 import com.paperize.paperize_server.file.data.FileDto;
+import com.paperize.paperize_server.folder.FolderEntity;
+import com.paperize.paperize_server.folder.data.FolderDto;
 import com.paperize.paperize_server.utils.S3Service;
 import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class FileMapper {
@@ -21,6 +25,24 @@ public class FileMapper {
                 .key(file.getKey())
                 .url(presignedUrl)
                 .createdAt(file.getCreatedAt().toString())
+                .build();
+    }
+
+    public FolderDto toFolderDto(FolderEntity folder) {
+        return com.paperize.paperize_server.folder.data.FolderDto.builder()
+                .id(String.valueOf(folder.getId()))
+                .userId(String.valueOf(folder.getUserId()))
+                .name(folder.getName())
+                .children(folder.getChildren().stream()
+                        .map(this::toFolderDto)
+                        .collect(Collectors.toList())
+                )
+                .files(folder.getFiles().stream()
+                        .map(this::toFileDto)
+                        .collect(Collectors.toList())
+                )
+                .createdAt(folder.getCreatedAt().toString())
+                .updatedAt(folder.getUpdatedAt().toString())
                 .build();
     }
 }
