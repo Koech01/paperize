974316ae6f1286e32b56b83f945b6e0e27c58ec3
Koech01 <01koech@gmail.com>
2025-03-23 21:35:10 +0300

    Adding signup component form logic

--- paperize-frontend/src/components/SignUp/index.tsx
+++ paperize-frontend/src/components/SignUp/index.tsx
@@ -1,15 +1,20 @@
 import css from '../Login/index.module.css';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { SyntheticEvent, useEffect, useState } from 'react'; 
 import authErrorLightIcon from '../assets/authErrorLightIcon.svg';
 import authSuccessLightIcon from '../assets/authSuccessLightIcon.svg';
 
 
 const SignUp = () => {
-
-    const [theme, setTheme] = useState('light'); 
-    const navigate          = useNavigate();
-    const redirectToLogin   = () => { navigate('/login/') };
+       
+    const [theme, setTheme]       = useState('light'); 
+    const [error, setError]       = useState('');
+    const [email, setEmail]       = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState(''); 
+    const navigate                = useNavigate();
+    const [redirect, setRedirect] = useState(false);
+    const redirectToLogin         = () => { navigate('/login/') };
 
 
     useEffect(() => {
@@ -18,28 +23,84 @@ const SignUp = () => {
     }, [])
 
 
+    const submit = async(e : SyntheticEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('', {
+                method      : 'POST',
+                headers     : {},
+                credentials : 'include',
+                body        : JSON.stringify({ username, email, password })
+            })
+
+            if (response.ok) {
+                setRedirect(true);
+            }
+
+            else {
+                const data = await response.json();
+                setError(data.detail);
+            }
+        }
+
+        catch (error) {
+            setError('INvalid');
+        }
+    }
+
+
+    useEffect(() => { if (redirect) { navigate('/'); } });
+
+
     return(
         
         <div className={`${css.authParentDiv} ${theme === 'light' ? css.lightTheme : css.darkTheme}`}> 
-            <form className={css.authForm}>
+            <form className={css.authForm} onSubmit={submit}>
                 <h2>Create Account</h2>
-                <input className={css.authFormInput} required autoComplete="off" placeholder="Username" type="text"/>
-                <input className={css.authFormInput} required autoComplete="off" placeholder="Email" type="email"/>
-                <input className={css.authFormInput} required autoComplete="off" placeholder="Password" type="password"/>
-                <button className={css.authFormBtn} type="submit">Sign up</button>
+                <input  
+                    required 
+                    autoComplete = "off" 
+                    placeholder  = "Username" 
+                    type         = "text"
+                    className    = {css.authFormInput}  
+                    onChange     = {(e) => setUsername(e.target.value) }
+                />
+
+                <input  
+                    required 
+                    autoComplete = "off" 
+                    placeholder  = "Email" 
+                    type         = "email"
+                    className    = {css.authFormInput} 
+                    onChange     = {(e) => setEmail(e.target.value) }
+                />
+
+                <input  
+                    required 
+                    autoComplete = "off" 
+                    placeholder  = "Password" 
+                    type         = "password"
+                    className    = {css.authFormInput} 
+                    onChange     = {(e) => setPassword(e.target.value) }
+                />
+
+                <button className={css.authFormBtn} type='submit'>Sign up</button>
                 <div className={css.authFormLinkDiv}><a className={css.authFormLink} onClick={redirectToLogin}>Log In</a></div>
 
-                <div className={css.authFormMessageParentDiv}> 
-                    <div className={`${css.authFormMessageDiv} ${css.authSuccess}`}>
-                        <img className={css.authFormSuccessIcon} src={authSuccessLightIcon} alt='auth-message-icon'/> 
-                        <p className={css.authFormMessageText}>Successfully created an account.</p>
-                    </div> 
+                {error && ( 
+                    <div className={css.authFormMessageParentDiv}> 
+                        <div className={`${css.authFormMessageDiv} ${css.authSuccess}`}>
+                            <img className={css.authFormSuccessIcon} src={authSuccessLightIcon} alt='auth-message-icon'/> 
+                            <p className={css.authFormMessageText}>Successfully created an account.</p>
+                        </div> 
 
-                    <div className={`${css.authFormMessageDiv} ${css.authError}`}>
-                        <img className={css.authFormSuccessIcon} src={authErrorLightIcon} alt='auth-message-icon'/> 
-                        <p className={css.authFormMessageText}>Successfully created an account.</p>
+                        <div className={`${css.authFormMessageDiv} ${css.authError}`}>
+                            <img className={css.authFormSuccessIcon} src={authErrorLightIcon} alt='auth-message-icon'/> 
+                            <p className={css.authFormMessageText}>Successfully created an account.</p>
+                        </div>
                     </div>
-                </div>
+                )}
             </form> 
         </div>
     );
