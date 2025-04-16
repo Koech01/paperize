044d756ffb7a99d91db2297ffde442aa4f0d0cad
Koech01 <01koech@gmail.com>
2025-04-16 14:41:39 +0300

    Creating recent document templates.

--- paperize-frontend/src/components/Upload/index.tsx
+++ paperize-frontend/src/components/Upload/index.tsx
@@ -1,63 +1,13 @@
 import css from './index.module.css';
 import { useEffect, useState } from 'react';
 import docIcon from '../assets/docIcon.svg';
-import closeLightIcon from '../assets/closeLight.svg'; 
 import cloudIcon from '../assets/cloud.png';
-
-import sevenzIcon from '../assets/7z.png';
-import aacIcon from '../assets/aac.png';
-import aviIcon from '../assets/avi.png';
-import bmpIcon from '../assets/bmp.png';
-import cssIcon from '../assets/css.png';
-import csvIcon from '../assets/csv.png';
-import datIcon from '../assets/dat.png';
-import docxIcon from '../assets/docx.png';
-import dxfIcon from '../assets/dxf.png';
-import ebookIcon from '../assets/ebook.png';
-import sepubIcon from '../assets/epub.png';
-import fileIcon from '../assets/file.png';
-import flacIcon from '../assets/flac.png';
-import flvIcon from '../assets/flv.png';
-import gifIcon from '../assets/gif.png';
-import gzIcon from '../assets/gz.png';
-import htmlIcon from '../assets/html.png';
-import iniIcon from '../assets/ini.png';
-import javaIcon from '../assets/java.png';
-import jpegIcon from '../assets/jpeg.png';
-import jpgIcon from '../assets/jpg.png';
-import jsIcon from '../assets/js.png';
-import jsonIcon from '../assets/json.png';
-import logIcon from '../assets/log.png';
-import mdIcon from '../assets/md.png';
-import mkvIcon from '../assets/mkv.png';
-import mp4Icon from '../assets/mp4.png';
-import odpIcon from '../assets/odp.png';
-import odsIcon from '../assets/ods.png';
-import odtIcon from '../assets/odt.png';
-import oggIcon from '../assets/ogg.png';
-import pngIcon from '../assets/png.png';
-import pptIcon from '../assets/ppt.png';
-import pptxIcon from '../assets/pptx.png';
-import pyIcon from '../assets/py.png';
-import rarIcon from '../assets/rar.png';
-import rtfIcon from '../assets/rtf.png';
-import sqlIcon from '../assets/sql.png';
-import svgIcon from '../assets/svg.png';
-import tarIcon from '../assets/tar.png';
-import tiffIcon from '../assets/tiff.png';
-import txtIcon from '../assets/txt.png';
-import wmvIcon from '../assets/wmv.png';
-import xlsIcon from '../assets/xls.png';
-import xlsxIcon from '../assets/xlsx.png';
-import xmlIcon from '../assets/xml.png';
-import yamlIcon from '../assets/yaml.png';
-import zipIcon from '../assets/zip.png';
+import closeLightIcon from '../assets/closeLight.svg'; 
 
 
 const Upload = () => {
 
-    const [theme, setTheme]               = useState('light');
-    // const [selectedFile, setSelectedFile] = useState< File | null>(null);
+    const [theme, setTheme] = useState('light');
 
 
     useEffect(() => {
